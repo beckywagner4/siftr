ff1cb99dcc82773ea9f12b4e55b69ec21e5e3b3f
Jack Davison <jd1184@york.ac.uk>
2021-04-23 11:31:46 +0100

    Add "read many SIFT" function
    Added a function to read multiple SIFT files at once, outputting a user-specified table.

---
+++ R/read_many_sift.R
@@ -0,0 +1,30 @@
+#' Read Many SIFT Files
+#'
+#' @description This function is a wrapper around "read_sift" to read in multiple SIFT .csv files. The user must specify a specific table within the SIFT data (defaulting to "concentrations") and the function will output a single tibble with that table from all provided .csv files, tagged with the start time from each.
+#'
+#' @param files A vector of file paths.
+#' @param table A table from the SIFT data. One of "time", "meta", "prep_phase", "sample_phase", "phase_mean_values", "intensity_corrected", "time_vs_mass", "concentrations", "analytes" or "summary". Defaults to "concentrations".
+#'
+#' @return A tibble.
+#' @export
+#'
+
+read_many_sift = function(files, table = "concentrations"){
+
+  read_specific = function(file, table = table){
+
+    all_sift = siftr::read_sift(file = file)
+
+    single_table = all_sift[[table]] %>%
+      dplyr::mutate(start_time = all_sift$time) %>%
+      dplyr::relocate(start_time)
+
+    return(single_table)
+
+  }
+
+  mapped_sift = purrr::map_dfr(.x = files, .f = ~read_specific(file = .x, table = table))
+
+  return(mapped_sift)
+
+}
