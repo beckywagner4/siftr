0161a6d90dd83ae929ea1df2081dce02e87f12bc
Jack Davison <jd1184@york.ac.uk>
2021-08-15 16:48:36 +0100

    Add package dependencies

---
+++ R/siftr-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"siftr"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
